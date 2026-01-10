994c256093ac69e5c1368f65635c4a6a0a1a00c5
Desertelk <cadenwatts111@gmail.com>
2026-01-10 13:36:58 -0500

    Exercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please input the grade you received: ");
+        String userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+        char letterGrade; 
+        if (grade >= 90)
+        {
+            letterGrade = 'A';
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letterGrade = 'B';
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letterGrade = 'C';
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letterGrade = 'D';
+        }
+        else
+        {
+            letterGrade = 'F';
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"You passed with a grade of {letterGrade}");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {letterGrade} study more for the next one");
+        }
     }
 }
