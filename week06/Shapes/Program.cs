1f76aef87063f92eb6d8623df456da304019eb1b
Desertelk <cadenwatts111@gmail.com>
2026-02-16 20:21:37 -0500

    Shapes learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s = new Square(5, "blue");
+
+        Rectangle r = new Rectangle("Red", 3.5, 2);
+
+        Circle c = new Circle("Purple", 2);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape);
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
     }
 }
