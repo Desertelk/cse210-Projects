59c03f30f73f5b5797d6e63d70c2cef4d7648ed6
Desertelk <cadenwatts111@gmail.com>
2026-02-16 21:12:53 -0500

    small changes.

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,8 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to your Goals!");
+        Console.WriteLine("Welcome to Eternal Quest!");
+
         GoalManager gm = new GoalManager();
         gm.Start();
+
+        Console.WriteLine("\nThanks for playing Eternal Quest!");
     }
 }
